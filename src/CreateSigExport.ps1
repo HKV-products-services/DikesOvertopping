9d1f0e6896354de1bd7ff39b9beab7f73f694810
Joël van den Berg <Joel.vandenBerg@deltares.nl>
2023-10-18 10:33:41 +0200

    Create source archive from src subdir

--- src/CreateSigExport.ps1
+++ src/CreateSigExport.ps1
@@ -1,2 +1,2 @@
 Remove-Item ".\DikesOvertopping_*.zip"
-git archive --output ".\DikesOvertopping_source_$(get-date -Format yyyyMMdd).zip" --prefix="DikesOvertopping_source/" HEAD .
+git archive --output ".\DikesOvertopping_source_$(get-date -Format yyyyMMdd).zip" --prefix="DikesOvertopping_source/" HEAD:src .
