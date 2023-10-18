ab2f67886b9fbbd78f98ff0d89c90e5201bee530
Joël van den Berg <Joel.vandenBerg@deltares.nl>
2023-10-18 09:32:54 +0200

    Update CreateSigExport for Git

--- src/CreateSigExport.ps1
+++ src/CreateSigExport.ps1
@@ -1,4 +1,2 @@
-Remove-Item ..\*.zip
-if ( Test-Path '..\DikesOvertopping_source' -PathType Container ) { Remove-Item -recurse -path ..\DikesOvertopping_source}
-svn export .\ ..\DikesOvertopping_source
-Compress-Archive ..\DikesOvertopping_source\ -DestinationPath ('..\' + 'DikesOvertopping_source_' + (get-date -Format yyyyMMdd) + '.zip')
+Remove-Item ".\DikesOvertopping_*.zip"
+git archive --output ".\DikesOvertopping_source_$(get-date -Format yyyyMMdd).zip" --prefix="DikesOvertopping_source/" HEAD .
