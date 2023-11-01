345725819f725c3f4d87493cc079d6ca94286e02
Joël van den Berg <Joel.vandenBerg@deltares.nl>
2023-11-01 11:01:11 +0100

    Remove unused doxygen files.

--- doc/doxygen/RunDoxygen.bat
+++
@@ -1,5 +0,0 @@
-doxygen DikesOvertoppingFortran.doxyfile > Doxygen.log
-
-call ".\latex\make.bat" >> Doxygen.log
-
-copy ".\latex\refman.pdf" "..\Dikes Overtopping Kernel - Technical Documentation.pdf" >> Doxygen.log
